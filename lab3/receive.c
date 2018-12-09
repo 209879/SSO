54b4beadcf5596027f096affee3d627d8709790b
luzna <wluzna@gmail.com>
2018-12-09 19:48:12 +0100

    receive only type 1 - longer than 5 chars

--- lab3/receive.c
+++ lab3/receive.c
@@ -4,24 +4,30 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
+typedef struct msgbuf {
+  long mtype;
+  char mtext[64];
+} message_buff;
+
 int
 main(void)
 {
   int msg_queue;   
   key_t k = ftok("/var", 'c');
+  message_buff msg_1;
   
   if(k != -1) { 
     msg_queue = msgget(k, 0644 | IPC_CREAT);
     if(msg_queue != -1) {
-      char buff[64] = ""; 
+      //char buff[64] = ""; 
 
       while(1) { 
 
-        if(msgrcv(msg_queue, &buff, sizeof(buff), 0, 0) != -1) {
+        if(msgrcv(msg_queue, &msg_1, sizeof(msg_1), 1, 0) != -1) {
           
-          if(strcmp(buff, "stop") == 0) 
+          if(strcmp(msg_1.mtext, "stop") == 0) 
             break;
-          printf("%s\n", buff); 
+          printf("%s typ: %ld \n", msg_1.mtext, msg_1.mtype); 
         } 
         else {
           perror("msgrcv()");
--- lab3/send.c
+++ lab3/send.c
@@ -4,26 +4,38 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
+typedef struct msgbuf {
+	long mtype;
+	char mtext[64];
+} message_buff;
+
 int
 main(void)
 {
   int msg_queue; 
   key_t k = ftok("/var", 'c');
-  
+  message_buff msg_1;
+
   if(k != -1) { 
 
     msg_queue = msgget(k, 0644 | IPC_CREAT);
     if(msg_queue != -1) {
 
-      char buff[64] = "";
+      //char buff[64] = "";
       size_t length; 
 
-      while(fgets(buff, sizeof(buff), stdin) != NULL) {
-        if((length = strlen(buff)) > 0) { 
+      while(fgets(msg_1.mtext, sizeof(msg_1.mtext), stdin) != NULL) {
+        if((length = strlen(msg_1.mtext+1)) > 0) { 
+        	if(length > 5)
+        		msg_1.mtype = 1;
+        	else
+        		msg_1.mtype = 2;
+        		printf("typ: %ld\n", msg_1.mtype );
 
-          if(buff [length - 1] == '\n') { buff [length - 1] = 0; length--; }
+
+          if(msg_1.mtext [length - 1] == '\n') { msg_1.mtext [length - 1] = 0; length--; }
         
-          if(msgsnd(msg_queue, &buff, length, 0) == -1) {
+          if(msgsnd(msg_queue, &msg_1, length, IPC_NOWAIT) == -1) {
             perror("msgsnd()");
             break;
           }

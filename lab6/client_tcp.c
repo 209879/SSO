de805b8cfdfead0ca2b1dd5d01ebc1ef311783dc
luzna <wluzna@gmail.com>
2018-12-10 01:40:37 +0100

    tcp - parameters check

--- lab6/client_tcp.c
+++ lab6/client_tcp.c
@@ -18,14 +18,26 @@ int main()
     struct sockaddr_in servaddr, cli; 
 
     socket_fd = socket(AF_INET, SOCK_STREAM, 0); 
- 
+    if (socket_fd == -1) { 
+        printf("socket not created \n"); 
+        exit(0); 
+    } 
+    else
+        printf("socket created \n"); 
     bzero(&servaddr, sizeof(servaddr)); 
   
     servaddr.sin_family = AF_INET; 
     servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
     servaddr.sin_port = htons(PORT); 
   
-    connect(socket_fd, (SA*)&servaddr, sizeof(servaddr));
+    if (connect(socket_fd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
+        printf("connection fail \n"); 
+        exit(0); 
+    } 
+    else
+        printf("connected to server \n");
+
+    pause();
 
     close(socket_fd); 
 } 
--- lab6/server_tcp.c
+++ lab6/server_tcp.c
@@ -20,6 +20,12 @@ int main()
  
 
     socket_fd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (socket_fd == -1) { 
+        printf("socket not created \n"); 
+        exit(0); 
+    } 
+    else
+        printf("socket created \n"); 
     bzero(&servaddr, sizeof(servaddr)); 
   
 
@@ -28,13 +34,32 @@ int main()
     servaddr.sin_port = htons(PORT); 
   
 
-    bind(socket_fd, (SA*)&servaddr, sizeof(servaddr)); 
-    listen(socket_fd, 5);
+    if ((bind(socket_fd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
+        printf("socket bind fail\n"); 
+        exit(0); 
+    } 
+    else
+        printf("socket binded..\n"); 
+  
+
+    if ((listen(socket_fd, 5)) != 0) { 
+        printf("listen failed\n"); 
+        exit(0); 
+    } 
+    else
+        printf("server listening \n"); 
     length = sizeof(cli); 
   
-    connection_fd = accept(socket_fd, (SA*)&cli, &length);  
-    if (connection_fd >= 0)
-        printf("success");
+ 
+    connection_fd = accept(socket_fd, (SA*)&cli, &length); 
+    if (connection_fd < 0) { 
+        printf("client accept fail \n"); 
+        exit(0); 
+    } 
+    else
+        printf("server acccepted client\n"); 
+
+    pause();
   
     close(socket_fd); 
 } 
